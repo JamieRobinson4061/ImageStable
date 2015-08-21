52ede70fdaa587ec57acd5778d733ce9f00fc15a
James Robinson <j@mesrobinson.co.uk>
2015-08-21 14:03:47 +0100

    update to second image stabilization project

--- ImageStable2/ImageStable2/ImageStable2.cpp
+++ ImageStable2/ImageStable2/ImageStable2.cpp
@@ -1,9 +1,15 @@
-#include <opencv2\opencv.hpp>
+//#include <opencv2\opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 #include <cassert>
 #include <cmath>
 #include <fstream>
 #include "stdafx.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "opencv\cv.hpp"
 
 using namespace std;
 using namespace cv;
